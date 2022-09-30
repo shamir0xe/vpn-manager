317226aceb1076a0c18500b21dbd39168d5a770d
shamir0xe <shamir0xe@gmail.com>
2022-09-30 18:13:41 +0330

    debug: modified

--- src/mediators/client_mediator.py
+++ src/mediators/client_mediator.py
@@ -42,55 +42,64 @@ class ClientMediator:
         try:
             # self.__log.add_log(f'trying to...', 'login')
             print('trying to connect')
-            # username, password = (Config.read('env.server.username'), Config.read('env.server.password'))
-            self.writer.write_line('Hello cruel world')
-            response = self.reader.next_line().strip()
-            print(f'response: {response}')
-            # self.__log.add_log(response, 'login-response')
-            self.status = False
-            return self
+            username, password = (Config.read('env.server.username'), Config.read('env.server.password'))
+            # self.writer.write_line('Hello cruel world')
+            # response = self.reader.next_line().strip()
+            # print(f'response: {response}')
+            # # self.__log.add_log(response, 'login-response')
+            # self.status = False
+            # return self
 
-            self.writer.write(
-                f'{username}\n{password}\n'
+            self.writer.write_line(
+                f'{username}\n{password}'
             )
             # self.writer.write_line(username)
             # self.writer.write_line(password)
             response = self.reader.next_line().strip()
-            self.__log.add_log(response, 'login-response')
+            # self.__log.add_log(response, 'login-response')
+            print(f'reponse: {response}')
             if response != 'OK':
                 self.status = False
-                self.__log.add_log(f'error: {response}', 'login')
+                print(f'error: {response}')
+                # self.__log.add_log(f'error: {response}', 'login')
         except BaseException as err:
             self.status = False
-            self.__log.add_log(f'error: {err}', 'login')
+            print(f'exception error: {err}')
+            # self.__log.add_log(f'error: {err}', 'login')
         return self
     
     def request_modification(self, port: int, gate_port: int) -> ClientMediator:
         if not self.status:
             return self
         try:
-            self.__log.add_log('sending request', 'request-modification')
+            # self.__log.add_log('sending request', 'request-modification')
+            print(f'sending request')
             self.writer.write_line(
                 f"{ServerCommands.RUN_MODIFICATION.value} --gate --interface.port {gate_port} --peer.port {port}"
             )
             response = self.reader.next_line().strip()
-            self.__log.add_log(f'response: {response}', 'request-modification')
+            # self.__log.add_log(f'response: {response}', 'request-modification')
+            print(f'response: {response}')
             if response != 'OK':
                 self.status = False
-                self.__log.add_log(f'error: {response}', 'request-modification')
+                print(f'error: {response}')
+                # self.__log.add_log(f'error: {response}', 'request-modification')
         except BaseException as err:
             self.status = False
-            self.__log.add_log(f'error: {err}', 'request-modification')
+            print(f'error: {err}')
+            # self.__log.add_log(f'error: {err}', 'request-modification')
         return self
 
     def request_end(self) -> ClientMediator:
         if not self.status:
             return self
         try:
-            self.__log.add_log('requesting end', 'request-end')
+            # self.__log.add_log('requesting end', 'request-end')
+            print(f'requesting end')
             self.writer.write_line(f"{ServerCommands.END.value}")
             response = self.reader.next_line().strip()
-            self.__log.add_log(response, 'request-end')
+            print(f'response: {response}')
+            # self.__log.add_log(response, 'request-end')
         except BaseException as err:
             self.status = False
         return self
